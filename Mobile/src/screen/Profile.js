ef4ea4f7b11dca7cc66e68c12eba260b44c456ae
julie-lambert <julie.lambert@laplateforme.io>
2024-07-15 16:35:34 +0200

    feat: Add navigation and screens for Home and Profile

---
+++ Mobile/src/screen/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View} from 'react-native'
+
+
+function Profile() {
+  return (
+    <View>
+      
+      <Text>Test</Text>
+    </View>
+  )
+}
+
+export default Profile
