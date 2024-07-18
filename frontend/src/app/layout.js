db6ab007b4dc35ef60cf24a98e181b7b089c4999
Clément Ramos <c.ramos@MacBook-Air-de-Clement.local>
2024-07-18 15:20:19 +0200

    Adding All the pages needed linked to the global.css

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavComponent from "@/components/NavComponent";
+import FooterComponent from "@/components/FooterComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>
+        <NavComponent />
         {children}
+        <FooterComponent />
       </body>
     </html>
   );
