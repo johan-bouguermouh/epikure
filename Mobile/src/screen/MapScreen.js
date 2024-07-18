1f104c21fdfa529c2b7d3f6103f57f4fd4946493
thomas-spinec <thomas.spinec@laplateforme.io>
2024-07-18 16:06:46 +0200

    FEAT: install react native maps to display the map on mapScreen

--- Mobile/src/screen/MapScreen.js
+++ Mobile/src/screen/MapScreen.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 function MapScreen({ navigation }) {
   return (
-    <View>
-      <Text>Test</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 43.3,
+          longitude: 5.4,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 43.3, longitude: 5.4 }}
+          title="Magasin"
+          description="Magasin de produits locaux"
+        />
+      </MapView>
+      {/* <Text>Map</Text>
       <Button
         title="Go to Magasin"
         onPress={() => navigation.push("Magasin")}
-      />
+      /> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default MapScreen;
