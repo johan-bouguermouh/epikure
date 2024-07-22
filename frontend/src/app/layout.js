2b95dab1943b25076b623347560e9adb64666a3c
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-07-22 11:12:10 +0200

    FEAT: Add SideBarComponent with ui and dependences

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,3 +1,7 @@
+import DashboardHeaderComponent from "@/components/DashboardHeaderComponent";
+import SideBarComponent from "@/components/SidebarComponent";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { SidebarProvider } from "@/context/SidebarContext";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -5,14 +9,30 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "EPIKURE",
-  description: "Application de mise en relation producteur(trice) et consomm'acteur",
+  description:
+    "Application de mise en relation producteur(trice) et consomm'acteur",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SidebarProvider>
+            <div className="flex min-h-screen w-full">
+              <SideBarComponent />
+              <div className="flex-1 flex flex-col">
+                <DashboardHeaderComponent />
+                <main className="flex-1">{children}</main>
+              </div>
+            </div>
+          </SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
