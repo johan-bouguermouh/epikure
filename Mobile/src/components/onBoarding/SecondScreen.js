40a19eaec326fc023bcdd5bac445addc3256615a
julie-lambert <julie.lambert@laplateforme.io>
2024-07-22 10:55:29 +0200

    feat : ajout du onboarding

--- Mobile/src/components/onBoarding/SecondScreen.js
+++ Mobile/src/components/onBoarding/SecondScreen.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View,StyleSheet,Text, Image } from "react-native";
+import {vh} from 'react-native-expo-viewport-units';
+import illustration_4 from '../../../assets/illustration_4.png';
 
-function SecondScreen({ step }) {
+function FirstScreen({ step }) {
   return (
-    <View>
-      <Text>Second Screen</Text>
-      <Text>Step: {step}</Text>
+    
+    <View style={styles.container}>
+      <Image source={illustration_4} style={styles.image} />
+      <Text style={styles.text}><Text style={{ fontWeight: 'bold', color: "#913C91"}}>Adoptez une alimentation de saison </Text>: suivez le calendrier des fruits et légumes avec notre application.</Text>
     </View>
   );
 }
 
-export default SecondScreen;
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    
+    height: vh(80),
+    width: "80%",
+   }, 
+  image: {
+    width: 450,
+    height: 450,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default FirstScreen;
