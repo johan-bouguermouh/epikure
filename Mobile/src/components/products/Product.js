8eadad19a879a97f2582cc7f2b50a5463d34ef72
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-07-23 10:35:50 +0200

    FEAT : add result on product page

--- Mobile/src/components/products/Product.js
+++ Mobile/src/components/products/Product.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Text, View } from "react-native";
-import { getProducts } from "../../services/product.service";
+import { getProduct } from "../../services/product.service";
+import { UserContext } from "../../contexts/UserContext";
 
-function Product() {
+// on récupère l'id du produit envoyer par la navigation et on le passe à la fonction getProducts
+
+function Product({ route }) {
+  const { productId } = route.params;
+  const { location, errorMsg } = useContext(UserContext);
+  console.log(location);
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    getProduct(productId, location).then((result) => {
+      setProduct(result);
+      setLoading(false);
+    });
+  }, [productId]);
+
   return (
     <View>
       <Text>Products</Text>
