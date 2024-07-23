446178c6fa258436df66d9e6b08542762b131b87
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-07-23 09:03:01 +0200

    Feat: add product screen

--- Mobile/src/screen/Produits.js
+++ Mobile/src/screen/Produits.js
@@ -21,28 +21,30 @@ function Produits({ navigation }) {
 
   return (
     <SafeAreaView style={{ padding: 12 }}>
-      <Text style={{ fontWeight: 700, marginVertical: 4 }}>
-        Si la saison s'étale du 1 ou 12{" "}
-      </Text>
-      <SeasonalFrieze harvestStartMounth={1} harvestEndMounth={12} />
-      <Text style={{ fontWeight: 700, marginVertical: 4 }}>
-        Si nous définissons pas de date, alors il prend la saison actuelle{" "}
-      </Text>
       <SeasonalFrieze />
-      <Text style={{ fontWeight: 700, marginVertical: 4 }}>
-        Si la saison s'étale sans coupure dans l'année{" "}
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          marginBottom: 12,
+          borderBottomColor: "#D9B3D9",
+          borderBottomWidth: 1,
+        }}
+      >
+        Les produits de saison
       </Text>
-      <SeasonalFrieze harvestStartMounth={6} harvestEndMounth={8} />
-      <Text style={{ fontWeight: 700, marginVertical: 4 }}>
-        Si la saison s'étale sur deux années{" "}
-      </Text>
-      <SeasonalFrieze harvestStartMounth={11} harvestEndMounth={3} />
-      <Text style={{ fontWeight: 700, marginVertical: 4 }}>
-        Si la saison d'un produit est d'un seul mois{" "}
-      </Text>
-      <SeasonalFrieze harvestStartMounth={3} harvestEndMounth={3} />
-      <Text>Produits</Text>
       <ProductGridComponent products={products} navigation={navigation} />
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          marginVertical: 12,
+          borderBottomColor: "#D9B3D9",
+          borderBottomWidth: 1,
+        }}
+      >
+        Les recettes de saison
+      </Text>
     </SafeAreaView>
   );
 }
